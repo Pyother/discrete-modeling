f8c2e0459a95e32c2c74e77e927ac6cfd38fc998
piotr <piotr.dominik.sobol@gmail.com>
2022-10-09 00:22:36 +0200

    Laboratory 1 - create Tkinter window

--- Laboratory_1_dimming_brigthening/project.py
+++ Laboratory_1_dimming_brigthening/project.py
@@ -8,12 +8,18 @@ img = cv2.imread('resources\\terrain_map_01.jpg')
 plt.imshow(img)
 plt.show()
 
-# Grid and 3D greyscale plot of the image:
-xx, yy = np.mgrid[0:img.shape[0], 0:img.shape[1]]
-fig = plt.figure()
-ax = fig.gca()
-ax = fig.add_subplot(111, projection='3d')
-plt.show()
+# Function for brightening:
+def threshold(img, thresh=200):
+        return((img > thresh) * 255).astype("uint8")
 
+# Function for dimming:
+def reverse_threshold(img, thresh=200):
+        return((img < thresh) * 255).astype("uint8")
+
+# Usage:
+plt.imshow(threshold(img), cmap='gray')
+plt.show()
+plt.imshow(reverse_threshold(img), cmap='gray')
+plt.show()
 
 
