7e3f3869283b04eb2631545ab48c1c8b3df4239c
piotr <piotr.dominik.sobol@gmail.com>
2022-10-31 12:09:21 +0100

    Laboratory 2 - set InstructionView

--- Laboratory_2_cellular_automaton/main.py
+++ Laboratory_2_cellular_automaton/main.py
@@ -1,4 +1,5 @@
 import arcade
+import time
 import utils.oscillator as osc
 
 ROW_COUNT = 25
@@ -10,11 +11,11 @@ SCREEN_WIDTH = (WIDTH + MARGIN) * COLUMN_COUNT + MARGIN
 SCREEN_HEIGHT = (HEIGHT + MARGIN) * ROW_COUNT + MARGIN
 SCREEN_TITLE = "Cellular Automata"
 
-class MyGame(arcade.Window):
+class GameView(arcade.View):
 
-    def __init__(self, width, height, title):
+    def __init__(self):
 
-        super().__init__(width, height, title)
+        super().__init__()
 
         self.grid = []
         for row in range(ROW_COUNT):
@@ -31,7 +32,7 @@ class MyGame(arcade.Window):
             for column in range(COLUMN_COUNT):
                 
                 if self.grid[row][column] == 1:
-                    color = arcade.color.NAPIER_GREEN
+                    color = arcade.color.COOL_BLACK
                 else:
                     color = arcade.color.WHITE
 
@@ -54,12 +55,36 @@ class MyGame(arcade.Window):
 
             if self.grid[row][column] == 0:
                 self.grid[row][column] = 1
+                self.grid[row+1][column] = 1
+                self.grid[row-1][column] = 1
             else:
                 self.grid[row][column] = 0
 
+class InstructionView(arcade.View):
+
+    def on_draw(self):
+        
+        self.clear()
+        arcade.draw_text("Cellular Automata", self.window.width / 2, self.window.height / 2,
+                         arcade.color.WHITE, font_size=40, anchor_x="center")
+        arcade.draw_text("Click to advance", self.window.width / 2, self.window.height / 2-75,
+                         arcade.color.WHITE, font_size=15, anchor_x="center")
+
+    def on_show_view(self):
+
+        arcade.set_background_color(arcade.csscolor.DARK_SLATE_BLUE)
+        arcade.set_viewport(0, self.window.width, 0, self.window.height)
+            
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        
+        game_view = GameView()
+        self.window.show_view(game_view)
+
 def main():
 
-    MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    start_view = InstructionView()
+    window.show_view(start_view)
     arcade.run()
 
 if __name__ == "__main__":
--- Laboratory_2_cellular_automaton/utils/oscillator.py
+++ Laboratory_2_cellular_automaton/utils/oscillator.py
@@ -5,8 +5,8 @@ class Oscilliator:
     def __init__(self, x, y):
         
         self.center = Cell(x, y)
-        self.up = self.center.y + 1
-        self.down = self.center.y - 1
+        self.up = Cell(x, y+1)
+        self.down = Cell(x, y-1)
 
     def __str__(self) -> str:
         print(f"Oscilliator grid coordinates: ({self.center.x}, {self.center.y})")
